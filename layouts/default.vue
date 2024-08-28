c4bd1d48744054bbb8ba3c1126eefcf9091c97a7
hyegyeonglee <wingedant1@naver.com>
2024-08-28 16:17:56 +0900

    초기셋팅 + header 추가

---
+++ layouts/default.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="flex flex-col min-h-screen">
+      <BaseSiteHeader :key="$route.fullPath"></BaseSiteHeader>
+      <slot/>
+      <div class="flex-grow justify-center"></div>
+      <BasePageFooter1 />
+    </div>
+  </template>
+  
+  <script setup lang="ts">
+  
+  </script>
+  
